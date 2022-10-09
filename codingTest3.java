e74839a6931527e7dc1609793a1a0bd6ce20f5e4
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2022-10-09 11:32:30 +0900

    Update codingTest3.java

--- codingTest3.java
+++ codingTest3.java
@@ -1,28 +1,34 @@
 class Solution {
-    public int solution(int[] numbers) {
-        int answer = 0;
-        int[] array=new int[10];
-        for(int i=0;i<array.length;i++){
-            array[i]=i;
-            answer=array[i]+answer;
-        } 
-
-        for(int i=0;i<numbers.length;i++){
-            for(int j=0;j<array.length;j++){
-                if(numbers[i]==array[j]) answer=answer-array[j];
+    public String solution(String s) {
+        char tmp='0';
+        String answer = "";
+        char[] array=s.toCharArray();
+        for(int i=0;i<s.length();i++) {
+            for(int j=i+1;j<s.length();j++) {
+            	if(array[j]>array[i]) {
+            		tmp=array[i];
+            		array[i]=array[j];
+            		array[j]=tmp;
+            	}
             }
         }
+        for(int i=0;i<s.length();i++) answer+=""+array[i];
         return answer;
     }
 }
 
-// 간결한 풀이
-// class Solution {
-//     public int solution(int[] numbers) {
-//         int sum = 45;   //0~9까지 더한 합
-//         for(int i:numbers){
-//             sum-=i;
-//         } 
-//         return answer;
+// import java.util.Arrays;  //모범 코드
+
+// public class ReverseStr {
+//     public String reverseStr(String str){
+//     char[] sol = str.toCharArray();
+//     Arrays.sort(sol);
+//     return new StringBuilder(new String(sol)).reverse().toString();
+//     }
+
+//     // 아래는 테스트로 출력해 보기 위한 코드입니다.
+//     public static void main(String[] args) {
+//         ReverseStr rs = new ReverseStr();
+//         System.out.println( rs.reverseStr("Zbcdefg") );
 //     }
 // }
