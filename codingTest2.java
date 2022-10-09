a97e717a2717f7db2857413dfca65b6e1f04be87
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2022-10-09 11:15:32 +0900

    Update codingTest2.java

--- codingTest2.java
+++ codingTest2.java
@@ -11,3 +11,16 @@ class Solution {
         return answer;
     }
 }
+
+
+// class StringExercise{.  // 모범 정답지
+//     String getMiddle(String word){
+
+//         return word.substring((word.length()-1) / 2, word.length()/2 + 1);    
+//     }
+//     // 아래는 테스트로 출력해 보기 위한 코드입니다.
+//     public static void  main(String[] args){
+//         StringExercise se = new StringExercise();
+//         System.out.println(se.getMiddle("power"));
+//     }
+// }
