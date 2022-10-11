309847b025530d593c21fa0dfabdf5a31f534f0d
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2022-10-11 14:38:28 +0900

    Update codingtest4Matches.java

--- codingtest4Matches.java
+++ codingtest4Matches.java
@@ -1,7 +1,14 @@
 class Solution {
-    public boolean solution(String s) {
-        
-        if(s.length()==4||s.length()==6)return s.matches("^[0-9]*$");
-        else return false;
-    }    
+    public int solution(int left, int right) {
+        int answer = 0;
+        int count=0;
+
+        for(int i=left;i<=right;i++) {
+            count=0;
+            for(int j=1;j<=i;j++) if(i%j==0) count++;      
+            if(count%2==0) answer+=i;
+            else answer-=i;
+        }       
+        return answer;
+    }
 }
