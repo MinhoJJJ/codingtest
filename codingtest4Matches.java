b1050d111c13bb72e07b16a8024c3fe9a3cae8fd
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2022-10-12 12:52:13 +0900

    Update codingtest4Matches.java

--- codingtest4Matches.java
+++ codingtest4Matches.java
@@ -1,21 +1,22 @@
 class Solution {
-    public int solution(int left, int right) {
-        int answer = 0;
-        int count=0;
-
-        for(int i=left;i<=right;i++) {
-            count=0;
-            for(int j=1;j<=i;j++) if(i%j==0) count++;      
-            if(count%2==0) answer+=i;
-            else answer-=i;
-        }       
-        return answer;
-    }
-}
-// class Solution {
-//     public boolean solution(String s) {
+    public boolean solution(String s) {
         
-//         if(s.length()==4||s.length()==6)return s.matches("^[0-9]*$");
-//         else return false;
-//     }    
+        if(s.length()==4||s.length()==6)return s.matches("^[0-9]*$");
+        else return false;
+    }    
+}
+
+// class Solution {
+//     public int solution(int left, int right) {
+//         int answer = 0;
+//         int count=0;
+
+//         for(int i=left;i<=right;i++) {
+//             count=0;
+//             for(int j=1;j<=i;j++) if(i%j==0) count++;      
+//             if(count%2==0) answer+=i;
+//             else answer-=i;
+//         }       
+//         return answer;
+//     }
 // }
