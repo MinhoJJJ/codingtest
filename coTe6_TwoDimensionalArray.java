275e3f8a639bfecb48d28c72f9a00a433d5411d4
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2022-10-12 13:35:42 +0900

    Update coTe6_TwoDimensionalArray.java

--- coTe6_TwoDimensionalArray.java
+++ coTe6_TwoDimensionalArray.java
@@ -1,15 +1,13 @@
-public class Test1 {
-	public static void main(String[] args) {
-		String a=args[0];
-		String b=args[1];
-		int ia=Integer.parseInt(a);
-		int ib=Integer.parseInt(b);
-		if(2<=ia&&ib<=9) {
-			for(int j=ia;j<=ib;j++) {
-				for(int i=1;i<=9;i++) {
-					System.out.println(j+"*"+i+"="+j*i);
-				}
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        
+        int[][] answer= new int[arr1.length][arr1[1].length];
+		
+		for(int i=0;i<arr1.length;i++){
+			for(int j=0;j<arr1[i].length;j++) {
+				answer[i][j]=arr1[i][j]+arr2[i][j];
 			}
 		}
-	}
+        return answer;
+    }
 }
