129de80894e31e0a9deff0f836d0775772765e82
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2022-10-13 22:53:04 +0900

    Update and rename coTe7_yet.java to coTe7_Mathmax().java

--- coTe7_Mathmax().java
+++ coTe7_Mathmax().java
@@ -13,3 +13,10 @@ class Solution {
         return answer;
     }
 }
+
+
+class Solution {
+    public long solution(long price, long money, long count) {
+        return Math.max(price * (count * (count + 1) / 2) - money, 0);
+    }
+}
