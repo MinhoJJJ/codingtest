d3563cbdc9ac09379f0130906de76432ea842c60
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2022-10-23 18:12:06 +0900

    Update coTe11_split()_contains() 복사본 5.java

--- coTe11_split()_contains() 복사본 5.java
+++ coTe11_split()_contains() 복사본 5.java
@@ -1,14 +1,31 @@
 class Solution {
-	public String solution(String s) {
+	public int solution(int[][] sizes) {
 
-		String answer = "";
-		int cnt = 0; // 홀짝 구분을 해줄 수
-		String[] array = s.split("");  // 그냥 문자열 하나를 통으로 가져온다.  상세설명은 아래
+		int[][] copySize=new int[sizes.length][2];  // 명함의 가장큰 부분, 가장 작은 부분을 넣을 배열
+		int max=0; // 최대값
+		int min=0; // 최소값
 
-		for(String ss : array) {  // foreach문으로 간결하게 표현
-			cnt = ss.contains(" ") ? 0 : cnt + 1;  // contains을 사용해서 공백이 한개든 두개든 확인 가능
-			answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); //홀짝 여부에 따라 대소문자로 바꿔줌
+		for(int i=0;i<sizes.length;i++) {
+			copySize[i][0]=Math.max(sizes[i][0],sizes[i][1]);  //  가로, 세로 중에 더 긴쪽을 넣어준다.
+			copySize[i][1]=Math.min(sizes[i][0],sizes[i][1]);  //  가로, 세로 중에 더 짧은 쪽을 넣어준다.
 		}
+		for(int i=0;i<sizes.length;i++) {
+			max=Math.max(copySize[i][0],max);   // 가장 긴 부분을 구해준다.
+			min=Math.max(copySize[i][1],min);   // 짧은쪽에서 가장 큰 부분을 구해준다.
+
+		}
+		int answer = max*min;    // 도출된 값 가로 * 세로 곱해준다.
 		return answer;
 	}
 }
+//class Solution {
+//	public int solution(int[][] sizes) {
+//		int length = 0, height = 0;
+//		for (int[] card : sizes) {   // sizes의 값들을 card[]에 넣어준다.
+//			length = Math.max(length, Math.max(card[0], card[1]));  가장 긴 부분을 구해준다.
+//			height = Math.max(height, Math.min(card[0], card[1]));  가장 짧은 부분을 구해준다.
+//		}
+//		int answer = length * height;
+//		return answer;
+//	}
+//}
