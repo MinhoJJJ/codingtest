4804e0bbceef9bfb2c09acfb35483856ceab8b35
JungMinHo <115209376+MinhoJJJ@users.noreply.github.com>
2022-10-29 16:46:38 +0900

    Update coTe4_Mathsqrt().java

--- coTe4_Mathsqrt().java
+++ coTe4_Mathsqrt().java
@@ -13,3 +13,14 @@ public class Test1 {
 		}
 	}
 }
+
+// class Solution {
+//    public int solution(int left, int right) {
+//        int answer = 0;
+
+//         for (int i=left;i<=right;i++) {
+//             if (i % Math.sqrt(i) == 0) answer -= i; //제곱수인 경우 약수의 개수가 홀수
+//             else answer +=i; //제곱수가 아닌 경우 약수의 개수가 짝수
+//         }
+//         return answer;
+//     }
